066b75292705f754df02f35b66c294e3737a0655
System Administrator <root@Alis-MacBook-Pro.local>
2019-09-26 11:09:06 +0500

    maps started

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
+
+export default class App extends React.Component {
+  state = {
+    region: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    }
+  };
+
+  getInitialState() {
+    return {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }
+    };
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  render() {
+    return (
+      <MapView style={{ width: "100%", flex: 1 }} region={this.state.region} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
